a7a8b2c9f817b200b77667c3117a76f4c4a59b69
Benito <dcostabenoit@gmail.com>
2021-03-25 22:57:01 +0100

    page 2 finished

--- js/page2.js
+++ js/page2.js
@@ -10,3 +10,53 @@ var swiper = new Swiper('.swiper-container', {
         delay:3000,
     },
   });
+
+  // HEADER
+
+
+window.onscroll = function() {myFunction(), color()};
+
+var headers= document.getElementById("myHeader");
+var sticky = headers.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    headers.classList.add("sticky");
+    console.log("ajouter")
+  } else {
+    headers.classList.remove("sticky");
+    console.log("relmove")
+  }
+}
+
+
+const header = document.querySelector(".header");
+const menu = document.querySelector(".menu");
+
+function color () {
+  var top = window.scrollY;
+  console.log(top);
+  if (top >= 50) {
+    console.log("add");
+    header.classList.add("active"), menu.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    menu.classList.remove("active");
+  }
+};
+
+
+const sr = ScrollReveal ({
+    duration : 3000,
+    mobile: false,
+    reset: true,
+});
+
+sr.reveal(".Perso", {
+    origin: 'top', 
+    // delay: 1000,
+   
+}, 1000);
+
+
+
